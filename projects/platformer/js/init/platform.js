3ce695f0dc2d0135743168f87bc28b70e035f1d2
David-Garnett <Davidgarnett48@gmail.com>
2021-01-20 20:26:19 +0000

    add platform

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -43,6 +43,7 @@
         createPlatform(600, 360);
         createPlatform(400, 530, 0.5);
         createPlatform(700, 635, 0.5);
+        createPlatform(350, 650, 3, 4);
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
