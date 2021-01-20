cf24b89593481d9a215d0a467465d16e8ca33d0e
David-Garnett <Davidgarnett48@gmail.com>
2021-01-20 20:32:59 +0000

    change argument

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -43,7 +43,7 @@
         createPlatform(600, 360);
         createPlatform(450, 535, 0.5);
         createPlatform(700, 635, 0.5);
-        createPlatform(700, 635, 3, 2);
+        createPlatform(695, 635, 3, 2);
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
